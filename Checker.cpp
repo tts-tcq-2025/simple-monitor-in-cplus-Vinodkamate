f7ea8ae78f1f29a463bc04350732c9505f3339df
Vinodkamate <vinodkamate4@gmail.com>
2025-08-04 19:40:14 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -1,25 +1,51 @@
-#include <assert.h>
 #include <iostream>
-using namespace std;
+#include <cassert>
+
+constexpr float MIN_TEMPERATURE = 0.0f;
+constexpr float MAX_TEMPERATURE = 45.0f;
+constexpr float MIN_SOC = 20.0f;
+constexpr float MAX_SOC = 80.0f;
+constexpr float MAX_CHARGE_RATE = 0.8f;
+
+static bool isTemperatureValid(float temperature) {
+    if (temperature < MIN_TEMPERATURE || temperature > MAX_TEMPERATURE) {
+        std::cout << "Temperature out of range!\n";
+        return false;
+    }
+    return true;
+}
+
+static bool isStateOfChargeValid(float soc) {
+    if (soc < MIN_SOC || soc > MAX_SOC) {
+        std::cout << "State of Charge out of range!\n";
+        return false;
+    }
+    return true;
+}
+
+static bool isChargeRateValid(float chargeRate) {
+    if (chargeRate > MAX_CHARGE_RATE) {
+        std::cout << "Charge Rate out of range!\n";
+        return false;
+    }
+    return true;
+}
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45||soc < 20 || soc > 80||chargeRate > 0.8){
-
-  if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
-    return false;
-  }
-  }
-  return true;
+    if (!isTemperatureValid(temperature)) {
+        return false;
+    }
+    if (!isStateOfChargeValid(soc)) {
+        return false;
+    }
+    if (!isChargeRateValid(chargeRate)) {
+        return false;
+    }
+    return true;
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
+    assert(batteryIsOk(25, 70, 0.7f) == true);
+    assert(batteryIsOk(50, 85, 0) == false);
+    return 0;
 }
